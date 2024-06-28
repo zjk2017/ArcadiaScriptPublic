dfa8e11daa150e486b594ff5b5e5975485edd2f0
zjk2017 <2548836866@qq.com>
2024-06-28 22:56:32 +0800

    Delete 沙发速运 directory
    用leaf本解密

--- 沙发速运/sfexpress.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-# -- coding: utf-8 --
-# -------------------------------
-# @Author : fix 20240627 ArcadiaScriptPublic  频道：https://t.me/ArcadiaScript 群组：https://t.me/ArcadiaScriptPublic leaf删库 上传老本 新本在偷撸
-# https://github.com/zjk2017/decode_action解密拉菲的脚本 解密后你看得懂的话就知道怎么弄了 so easy
