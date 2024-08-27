15b9fea2530c7a8624be2dd5e70c676970412461
zjk2017 <2548836866@qq.com>
2024-08-27 14:18:19 +0800

    Update common.py

--- common.py
+++ common.py
@@ -27,26 +27,26 @@ def make_request(url, json_data=None, method='get', headers=None):
 # 通义千问API
 def qianwen_messages(basic_question, question):
     content = ''
-    # qw_key = os.getenv("QIANWEN_KEY")
-    # if not qw_key:
-    #     print(f'⛔️未获取到通义千问key：请检查变量 {qw_key} 是否填写')
-    # else:
-    #     dashscope.api_key = qw_key
-    #     messages = [{'role': 'system', 'content': 'You are a helpful assistant.'},
-    #                 {'role': 'user', 'content': basic_question + question}]
-    #     response = dashscope.Generation.call(
-    #         dashscope.Generation.Models.qwen_turbo,
-    #         messages=messages,
-    #         seed=random.randint(1, 10000),
-    #         result_format='message',
-    #     )
-    #     if response.status_code == HTTPStatus.OK:
-    #         content = response['output']['choices'][0]['message']['content']
-    #     else:
-    #         print('Request id: %s, Status code: %s, error code: %s, error message: %s' % (
-    #             response.request_id, response.status_code,
-    #             response.code, response.message
-    #         ))
+    qw_key = os.getenv("QIANWEN_KEY")
+    if not qw_key:
+        print(f'⛔️未获取到通义千问key：请检查变量 {qw_key} 是否填写')
+    else:
+        dashscope.api_key = qw_key
+        messages = [{'role': 'system', 'content': 'You are a helpful assistant.'},
+                    {'role': 'user', 'content': basic_question + question}]
+        response = dashscope.Generation.call(
+            dashscope.Generation.Models.qwen_turbo,
+            messages=messages,
+            seed=random.randint(1, 10000),
+            result_format='message',
+        )
+        if response.status_code == HTTPStatus.OK:
+            content = response['output']['choices'][0]['message']['content']
+        else:
+            print('Request id: %s, Status code: %s, error code: %s, error message: %s' % (
+                response.request_id, response.status_code,
+                response.code, response.message
+            ))
     return content
 
 
