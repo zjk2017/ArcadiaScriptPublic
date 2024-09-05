df36c4696c62cf10a737188a402bd0e46413b1de
zjk2017 <2548836866@qq.com>
2024-09-06 01:46:07 +0800

    Update 好奇车生活抢兑自定义抢购次数.js

--- 好奇车生活抢兑自定义抢购次数.js
+++ 好奇车生活抢兑自定义抢购次数.js
@@ -32,9 +32,9 @@ async function main() {
     let exchangeNeedPoints = queryByPointsMallCardId.result.exchangePointsValue;
     let exchangeNeedMoney = queryByPointsMallCardId.result.exchangeMoneyValue;
     for (let i = 0; i < Cheryfs_MUM; i++) {
-        for (let j = 0; j < 2; j++) {
-            let exchange = commonGet(`/pointsmall/exchangeCard?pointsMallCardId=${pointsMallCardId}&exchangeCount=${exchangeCount}&mallOrderInputVoStr=%7B%22person%22:%22%22,%22phone%22:%22%22,%22province%22:%22%22,%22city%22:%22%22,%22area%22:%22%22,%22address%22:%22%22,%22remark%22:%22%22%7D&channel=1&exchangeType=${exchangeType}&exchangeNeedPoints=${exchangeNeedPoints}&exchangeNeedMoney=${exchangeNeedMoney}&cardGoodsItemIds=`)
-        }
+        // for (let j = 0; j < 2; j++) {
+        let exchange = commonGet(`/pointsmall/exchangeCard?pointsMallCardId=${pointsMallCardId}&exchangeCount=${exchangeCount}&mallOrderInputVoStr=%7B%22person%22:%22%22,%22phone%22:%22%22,%22province%22:%22%22,%22city%22:%22%22,%22area%22:%22%22,%22address%22:%22%22,%22remark%22:%22%22%7D&channel=1&exchangeType=${exchangeType}&exchangeNeedPoints=${exchangeNeedPoints}&exchangeNeedMoney=${exchangeNeedMoney}&cardGoodsItemIds=`)
+        // }
         await $.wait(100)
     }
     await $.wait(60000)
